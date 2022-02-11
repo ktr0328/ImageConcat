6bc9a48e136520046f7947c3117a322361a2cd22
ktr0328 <zf7heatpa@gmail.com>
2022-02-12 03:37:49 +0900

    fix: エラーをログに出力するように変更

--- src/util/convert_non_alpha.py
+++ src/util/convert_non_alpha.py
@@ -2,17 +2,21 @@ import os
 
 from pathlib import Path
 
+from loguru import logger
 from PIL import Image
 
 
-def convert(img_dir: Path, quality: int, logger):
-    pid = os.getpid()
-    files = img_dir.glob('*.png')
+def convert(img_dir: Path, quality: int):
+    try:
+        pid = os.getpid()
+        files = img_dir.glob('*.png')
 
-    for file in files:
-        im = Image.open(file)
-        im = im.convert('RGB')
-        new_file = file.parent / (file.stem + '.jpeg')
-        im.save(new_file, quality=quality)
-        file.unlink()
-        logger.trace(f'{pid}:convert to non_alpha: {new_file.resolve()}')
+        for file in files:
+            im = Image.open(file)
+            im = im.convert('RGB')
+            new_file = file.parent / (file.stem + '.jpeg')
+            im.save(new_file, quality=quality)
+            file.unlink()
+            logger.trace(f'{pid}:convert to non_alpha: {new_file.resolve()}')
+    except Exception as e:
+        logger.error(f'{img_dir} failed: {e}')
